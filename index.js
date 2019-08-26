99acdfd2e67619a8b041ec0bb735f38a79e9f1a5
Gary <alemmartin@gmail.com>
2019-08-26 12:41:34 -0300

    Implement prettier

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const app = express();
 const routes = require('./routes/index');
 app.use('/', routes);
 
-app.listen(process.env.PORT, () => console.log(`peermedical-api listening on port ${process.env.PORT}!`));
+app.listen(process.env.PORT, () =>
+    console.log(`peermedical-api listening on port ${process.env.PORT}!`)
+);
